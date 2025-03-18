a8785a936bd2507268898e9949303b22bbb7a2ca
parkgun6 <a01088807068@gmail.com>
2025-03-18 16:06:09 +0900

    feat: member ManyToMany 추가

--- src/main/java/com/phiworks/security/entity/Roles.java
+++ src/main/java/com/phiworks/security/entity/Roles.java
@@ -2,9 +2,13 @@ package com.phiworks.security.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="role")
@@ -19,6 +23,9 @@ public class Roles implements GrantedAuthority {
     private Long roleId;
 
     private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private final Set<Member> users = new HashSet<>();
 
     @Override
     public String getAuthority() {
